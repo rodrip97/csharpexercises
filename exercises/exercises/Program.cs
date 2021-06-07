a864df2579efb11a08f7a6f55608cc1721b5ce73
rodrip97 <zimmer672@gmail.com>
2021-06-07 10:10:03 -0400

    Fixed a little bug

--- exercises/exercises/Program.cs
+++ exercises/exercises/Program.cs
@@ -9,14 +9,15 @@ namespace exercises
         {
             int result = 0;
             System.Random rando = new System.Random();
-            int randoN = rando.Next(0,10);
+            int randoN = rando.Next(1,10);
             bool answer = false; //This chekcs if answer is correct or not, as it stands it is false
 
             while (answer == false)
              { 
 
             Console.WriteLine("Please guess a number between 1 and 10");
-            result = Convert.ToInt32((Console.Read()));
+                string result_value = Console.ReadLine();
+                result = Convert.ToInt32(result_value);
 
             if (result == Convert.ToInt32(randoN))
             {
